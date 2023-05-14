2443254b16d27de693d85b923531cafa06d0af89
Joe McNally <joe.t.mcnally@hotmail.com>
2023-05-14 17:28:33 +0100

    Add animator folder back in

---
+++ src/components/game/mechanics/helpers/graphics/animator/displayPleaseWait.test.js
@@ -0,0 +1,36 @@
+import Animator from "./animator";
+
+let ctx;
+let loadTint;
+
+describe("displayPleaseWait", () => {
+  beforeEach(() => {
+    ctx = {
+      globalAlpha: null,
+      font: null,
+      fillStyle: null,
+      textAlign: null,
+      textBaseline: null,
+      fillText: () => undefined,
+    };
+    loadTint = jest.fn();
+  });
+
+  it("calls loadTint to darken the board", () => {
+    Animator.displayPleaseWait(ctx, loadTint);
+    expect(loadTint).toHaveBeenCalledTimes(1);
+    expect(loadTint).toHaveBeenCalledWith(ctx);
+  });
+
+  it("renders 'Please wait...' text on the screen", () => {
+    jest.spyOn(ctx, "fillText");
+    Animator.displayPleaseWait(ctx, loadTint);
+    expect(ctx.globalAlpha).toBe(1);
+    expect(ctx.font).toBe("100px Arial");
+    expect(ctx.fillStyle).toBe("white");
+    expect(ctx.textAlign).toBe("center");
+    expect(ctx.textBaseline).toBe("middle");
+    expect(ctx.fillText).toHaveBeenCalledTimes(1);
+    expect(ctx.fillText).toHaveBeenCalledWith("Please wait...", 448, 496);
+  });
+});
